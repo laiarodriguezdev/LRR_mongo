584a7fa7785ad956cfc446884771d789a4100380
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-03-23 20:33:17 +0100

    Configuració de BBDD + connection Fast Api

---
+++ Model/Film.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class Film(BaseModel):
+    title:str
+    director:str 
+    year:int
+    genere:str
+    rating:int
+    country: str
---
+++ db/film.py
@@ -0,0 +1,8 @@
+import pymongo
+
+#AQUEST FITXER NOMÉS S'ENCARREGA DE CONECTARSE AMB LA BBDD. 
+def db():
+    try:
+        return pymongo.MongoClient("mongodb://localhost:27017/").films
+    except Exception as e:
+        print(f"ERROR BBDD: {e}")
