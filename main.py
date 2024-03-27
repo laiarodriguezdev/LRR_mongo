ad089e8b69fa38f7fa20a24d632081f64ae67cc2
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2024-03-27 17:08:57 +0100

    Refactor main. Now I directly return the function.

--- main.py
+++ main.py
@@ -9,53 +9,44 @@ app = FastAPI(title="Laia Rodríguez Ramos - CRUD + Consultes avançades")
 # def read_root():
 #     return {"Hello": "World"}
 
-
 #RETORNA TOTA LA LLISTA D'OBJECTES
 @app.get("/films")
 def getFilms():
-    data = filmPeticions.getAllFilms()
-    return data
+    return filmPeticions.getAllFilms()
 
 #RETORNA LA PELI CORRESPONENT AL ID
 @app.get("/films/{id}")
 def getFilmById(id):
-    data = filmPeticions.getFilmById(id)
-    return data
+    return filmPeticions.getFilmById(id)
 
 #CREA UN NOU DOCUMENT
 @app.post("/film/")
 def createFilm(film: Film):
-    data = filmPeticions.createFilm(film)
-    return data
+    return filmPeticions.createFilm(film)
 
 #ACTUALITZA UN NOU DOCUMENT/FILM. 
 @app.put("/film/{id}")
 def updateFilm(id, film:Film):
-    data=filmPeticions.updateFilm(id, film)
-    return data
+    return filmPeticions.updateFilm(id, film)
 
 #ESBORRA EL DOCUMENT/FILM. 
 @app.delete("/film/{id}")
 def deleteFilm(id):
-    data=filmPeticions.deleteFilm(id)
-    return data
+    return filmPeticions.deleteFilm(id)
 
 
 #RETORNA LES PELIS DE ACTION, BIOGRAPHY, ETC. 
 @app.get("/filmsGenre")
 def getFilmsByGenre(genre: str = "Action"):
-    data= filmPeticions.getFilmsByGen(genre)
-    return data
+    return filmPeticions.getFilmsByGen(genre)
 
 #RETORNA LES PELIS EN ORDRE ASC/DASC I SEGONS LA KEY. 
 @app.get("/filmsOrder")
 def getFilmsByOrder(field: str = "title", order: str = "asc"):
     userOrder = 1 if order == "asc" else -1
-    data = filmPeticions.getFilmsByOrder(field, userOrder)
-    return data
+    return filmPeticions.getFilmsByOrder(field, userOrder)
 
 #RETORNA TANTES PELIS COM L'USUARI VULGUI. 
 @app.get("/filmsLimit")
 def getFilmsByLimit(limit: int):
-    data = filmPeticions.getFilmsByLimit(limit)
-    return data
+    return filmPeticions.getFilmsByLimit(limit)
